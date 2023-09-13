95c519f90aee6ef4a65f36a77349d0d88bb97ddc
Admin <mishavolgodonsk@gmail.com>
2023-09-13 21:36:11 +0300

    1.9. Статические и нестатические методы [#504806]

---
+++ src/main/java/ru/job4j/oop/Calculator.java
@@ -0,0 +1,38 @@
+package ru.job4j.oop;
+
+public class Calculator {
+
+    private static int x = 5;
+
+    public static int sum(int a) {
+        return x + a;
+    }
+
+    public int multiply(int a) {
+        return x * a;
+    }
+
+    public static int minus(int a) {
+        return a - x;
+    }
+
+    public int divide(int a) {
+        return a / x;
+    }
+
+    public int sumAllOperation(int a) {
+        return sum(a) + multiply(a) + minus(a) + divide(a);
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int sumResult = sum(30);
+        int multiplyResult = calculator.multiply(30);
+        int minusResult = minus(30);
+        int divideResult = calculator.divide(30);
+        int sumAllResult = calculator.sumAllOperation(30);
+        System.out.println("If argument = 30:\n" + "sum = " + sumResult + "\n" + "multiply = " + multiplyResult
+                + "\n" + "minus = " + minusResult + "\n" + "divide = " + divideResult
+                + "\n" + "sumAllOperation = " + sumAllResult);
+    }
+}
