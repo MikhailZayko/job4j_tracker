d034ab45a22269a30c1193fac88e6f95ba8d92bd
Admin <mishavolgodonsk@gmail.com>
2023-10-13 18:18:03 +0300

    1. Интерфейс Queue [#504942]

---
+++ src/main/java/ru/job4j/queue/AppleStore.java
@@ -0,0 +1,36 @@
+package ru.job4j.queue;
+
+import java.util.Queue;
+
+public class AppleStore {
+    private final Queue<Customer> queue;
+
+    private final int count;
+
+    public AppleStore(Queue<Customer> queue, int count) {
+        this.queue = queue;
+        this.count = count;
+    }
+
+    public String getLastHappyCustomer() {
+        if (queue.isEmpty()) {
+            throw new RuntimeException("The queue cannot be empty");
+        }
+        String result = null;
+        for (int i = 0; i < count; i++) {
+            result = queue.poll().name();
+        }
+        return result;
+    }
+
+    public String getFirstUpsetCustomer() {
+        if (queue.isEmpty()) {
+            throw new RuntimeException("The queue cannot be empty");
+        }
+        String result = null;
+        for (int i = 0; i <= count; i++) {
+            result = queue.poll().name();
+        }
+        return result;
+    }
+}
