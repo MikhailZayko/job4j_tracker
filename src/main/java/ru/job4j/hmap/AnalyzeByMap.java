642a00e97f098fd8daa76c1a4d1270833445cfe4
Admin <mishavolgodonsk@gmail.com>
2023-10-26 00:19:34 +0300

    9. Лямбда выражения и Collection Framework [#505026]

--- src/main/java/ru/job4j/hmap/AnalyzeByMap.java
+++ src/main/java/ru/job4j/hmap/AnalyzeByMap.java
@@ -32,7 +32,7 @@ public class AnalyzeByMap {
         Map<String, Double> map = new LinkedHashMap<>();
         for (Pupil pupil : pupils) {
             for (Subject subject : pupil.subjects()) {
-                map.put(subject.name(), map.getOrDefault(subject.name(), 0D) + subject.score());
+                map.merge(subject.name(), (double) subject.score(), Double::sum);
             }
         }
         for (Map.Entry<String, Double> entry : map.entrySet()) {
@@ -64,7 +64,7 @@ public class AnalyzeByMap {
         Map<String, Double> map = new LinkedHashMap<>();
         for (Pupil pupil : pupils) {
             for (Subject subject : pupil.subjects()) {
-                map.put(subject.name(), map.getOrDefault(subject.name(), 0D) + subject.score());
+                map.merge(subject.name(), (double) subject.score(), Double::sum);
             }
         }
         for (Map.Entry<String, Double> entry : map.entrySet()) {
